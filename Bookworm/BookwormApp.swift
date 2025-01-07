4af9af3ea086a3b6baedd2636e54588a72ca841e
chiji1108 <shingo.pad@gmail.com>
2025-01-07 18:01:10 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by 千々岩真吾 on 2025/01/07.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
